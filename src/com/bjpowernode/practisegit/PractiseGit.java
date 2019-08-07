f9234f21ff9f87cee5e1f1bfb4309f0db2ef19ad
UserWcj <243673750@.com>
2019-08-07 22:24:30 +0800

    初始导入

---
+++ src/com/bjpowernode/practisegit/PractiseGit.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.practisegit;
+
+/**
+ * Classname:PractiseGit
+ * Package: com.bjpowernode.practisegit
+ * Description:
+ *
+ * @date:2019/8/7 22:04
+ * @author:wangcanjie
+ */
+public class PractiseGit {
+    public static void main(String[] args) {
+        System.out.println("已经入职！");
+    }
+}
